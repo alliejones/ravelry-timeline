cb2aa662b013aa41eef50e51e7f528c6dec21bd5
Allie Jones <allie.jones@gmail.com>
2012-07-21 21:40:21 -0400

    Refactoring, add project class

--- httpdocs/js/script.js
+++ httpdocs/js/script.js
@@ -22,7 +22,44 @@ var Timeline = function (data) {
 
 	self.canvas = null;
 
-	self.init = function() {
+	self.Project = function (start, end, data) {
+		this.name = data.name;
+		this.patternName = data.pattern ? data.pattern.name : null;
+
+		this.imageThumb = data.thumbnail ? data.thumbnail.src : null;
+		this.imageMed = data.thumbnail ? data.thumbnail.medium : null;
+		
+		this.startDate = start;
+		this.startDateHuman = start ? start.format('LL') : null;
+		this.startOffset = null;
+		this.startOffsetHuman = null;
+		this.endDate = end;
+		
+		this.duration = moment.duration(end.diff(start));
+		this.durationHuman = this.duration ? this.duration.asMonths() : null;
+	};
+
+	/*
+	   stackPosition is the zero-indexed timeline row the bar should be drawn in
+		 0 is the top row
+	*/
+	self.Project.prototype.drawBar = function (stackPosition) {
+		var width = self.config.monthWidth * this.duration.asMonths();
+
+		self.canvas.add(new fabric.Rect({
+			top: (stackPosition * (self.config.barHeight + self.config.barSpacing)) +
+					 self.config.barSpacing + self.config.barHeight/2,
+			// offset one month length from the left edge
+			left: ((this.startOffset.asMonths() + 1) * self.config.monthWidth) + (width/2),
+			/* projects that were started and completed on the same day will have
+				 a length of zero, so set a default width of 1-day-ish in that case */ 
+			width: width || self.config.monthWidth/30,
+			height: self.config.barHeight,
+			fill: 'rgba(124,145,222,.75)'
+		}));
+	}
+
+	self.init = function () {
 		self.initProjects();
 		self.initCanvas();
 		self.render();
@@ -35,23 +72,12 @@ var Timeline = function (data) {
 			var duration = null;
 
 			if (start !== null && end !== null) {
-				duration = moment.duration(end.diff(start));
 
 				// check if this project's completion date is the end of the timeline
 				if (self.data.endDate < end)
 					self.data.endDate = end;
 
-				self.data.projects.push({
-					name: proj.name,
-					patternName: proj.pattern ? proj.pattern.name : null,
-					imageThumb: proj.thumbnail ? proj.thumbnail.src : null,
-					imageMed: proj.thumbnail ? proj.thumbnail.medium : null,
-					startDate: start,
-					startDateHuman: start ? start.format('LL') : null,
-					endDate: end,
-					duration: duration,
-					durationHuman: duration ? duration.asMonths() : null
-				});
+				self.data.projects.push(new self.Project(start, end, proj));
 			}
 		});
 
@@ -108,28 +134,14 @@ var Timeline = function (data) {
 		}
 
 		_.each(self.data.projects, function(proj, i) {
-			var width = self.config.monthWidth * proj.duration.asMonths();
-
-			self.canvas.add(new fabric.Rect({
-				top: (self.config.barHeight * 1.2 * (i % self.config.barStackHeight)) + (self.config.barHeight * 1.2),
-				// offset one month length from the left edge
-				left: ((proj.startOffset.asMonths() + 1) * self.config.monthWidth) + (width/2),
-				/* projects that were started and completed on the same day will have
-					 a length of zero, so set a default width of 1-day-ish in that case */ 
-				width: width || self.config.monthWidth/30,
-				height: self.config.barHeight,
-				fill: 'rgba(124,145,222,.75)'
-			}));
+			proj.drawBar(i % 10);
 		});
 	};
-
-};
-
-Timeline.createTimeline = function (data) {
-	tl = new Timeline(data);
-	tl.init();
 };
 
 $(function() {
-	$.getJSON('/ravelry/httpdocs/data/progress-soph.json').success(Timeline.createTimeline);
+	$.getJSON('/ravelry/httpdocs/data/progress-soph.json').success(function(data) {
+		tl = new Timeline(data);
+		tl.init();
+	});
 });
